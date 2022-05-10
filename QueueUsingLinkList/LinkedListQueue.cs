2e50e1957916a730a47a3f2dfe8e9b1d34b0a8ea
suraj <soorajkrpandit@gmail.com>
2022-05-10 14:21:29 +0530

    Add project files.

---
+++ QueueUsingLinkList/LinkedListQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingLinkList
+{
+    internal class LinkedListQueue
+    {
+        Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                //node.next = head;
+
+            }
+            Console.WriteLine("{0} inserted into queue ", node.data);
+
+        }
+        internal void Peek()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the Queue ", this.head.data);
+        }
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value  is dequeue {0} ", this.head.data);
+            this.head = this.head.next;
+        }
+        internal void IsEmpty()
+        {
+            while (this.head != null)
+            {
+                Peek();
+                Dequeue();
+            }
+        }
+    }
+}
---
+++ QueueUsingLinkList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program for Queue");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Peek();
+
+            linkedListQueue.Dequeue();
+            linkedListQueue.IsEmpty();
+
+            Console.ReadLine();
+        }
+    }
+}
